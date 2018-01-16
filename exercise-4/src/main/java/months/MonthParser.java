2eeb0b3978961f78ccd79efdca16979455d8c723
Andreas Naustdal <and.epost@gmail.com>
2018-01-16 20:04:11 +0100

    Exercise 4

--- exercise-4/src/main/java/months/MonthParser.java
+++ exercise-4/src/main/java/months/MonthParser.java
@@ -4,6 +4,21 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class MonthParser {
     public Month parseMonth(int numberOfMonth) {
-        throw new NotImplementedException();
+        if (numberOfMonth < 1 || numberOfMonth > 12) throw new IllegalArgumentException();
+        switch (numberOfMonth) {
+            case 1:
+                return new Month("January", 31);
+            case 2:
+                return new Month("February", 28);
+            case 3:
+                return new Month("March", 31);
+            case 4:
+                return new Month("April", 30);
+            case 5:
+                return new Month("May", 31);
+            case 6:
+                return new Month("June", 30);
+        }
+        return null;
     }
 }
--- exercise-4/src/test/java/months/MonthParserTest.java
+++ exercise-4/src/test/java/months/MonthParserTest.java
@@ -1,9 +1,68 @@
 package months;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 public class MonthParserTest {
+    MonthParser monthParser;
+
+    @Before
+    public void initializeMonthParser() {
+        monthParser = new MonthParser();
+    }
+
+    @Test
+    public void parseJanuary() {
+        assertTrue(monthParser.parseMonth(1).name == "January");
+        assertEquals("Wrong number of days", monthParser.parseMonth(1).days,31);
+    }
+
+    @Test
+    public void parseFebruary() {
+        assertTrue(monthParser.parseMonth(2).name == "February");
+        assertEquals("Wrong number of days", monthParser.parseMonth(2).days, 28);
+    }
+
+    @Test
+    public void parseMarch() {
+        assertTrue(monthParser.parseMonth(3).name == "March");
+        assertEquals("Wrong number of days", monthParser.parseMonth(3).days, 31);
+    }
+
+    @Test
+    public void parseApril() {
+        assertTrue(monthParser.parseMonth(4).name == "April");
+        assertEquals("Wrong number of days", monthParser.parseMonth(4).days, 30);
+    }
+
+    @Test
+    public void parseMay() {
+        assertTrue(monthParser.parseMonth(5).name == "May");
+        assertEquals("Wrong number of days", monthParser.parseMonth(5).days,31);
+    }
+
+    @Test
+    public void parseJune() {
+        assertTrue(monthParser.parseMonth(6).name == "June");
+        assertEquals("Wrong number of days", monthParser.parseMonth(6).days, 30);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void canNotParseNegativeMonthNumber() {
+        monthParser.parseMonth(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void canNotParseZeroMonthNumber() {
+        monthParser.parseMonth(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void canNotParseTooHighMonthNumber() {
+        monthParser.parseMonth(13);
+    }
 
 }
